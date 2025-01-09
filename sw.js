0d43433537587fc70a4989b4a8681c71975295fd
anfego1 <anfego1@hotmail.com>
2025-01-09 16:51:16 +0000

    Cierre de sesión para empezar a capturar tokens de navegadores para notificaciones push

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'dosxdos103',
+const CACHE_NAME = 'dosxdos104',
   urlsToCache = [
     'https://dosxdos.app.iidos.com/index.html?utm_source=web_app_manifest',
     'https://dosxdos.app.iidos.com/index.html',
