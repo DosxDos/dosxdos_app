5d623f521991d4082cd5cc2fd6484e8118cd2458
anfego1 <anfego1@hotmail.com>
2025-02-21 13:46:53 +0000

    Funcionalidad Firebase en la página notificaciones.html

---
+++ js/utils/loadFirebaseNotificacionesPage.js
@@ -0,0 +1,85 @@
+//Este módulo es otra versión del módulo loadFirebase, pero específico sólo para las páginas que no tienen la campana de notificaciones.
+async function loadFirebase() {
+    try {
+
+        const userAgent = navigator.userAgent;
+        const isAndroid = /android/i.test(userAgent);
+        const isiOS = /(iphone|ipad|ipod)/i.test(userAgent);
+
+        if (isAndroid || isiOS) {
+            console.warn("Dispositivo Android o Ios, Firebase web push notifications no será cargado.");
+            return;
+        }
+
+        if (!navigator.onLine) {
+            console.warn("Sin conexión, Firebase web push notifications no será cargado.");
+            return;
+        }
+
+        console.warn("Conexión activa. Cargando Firebase web push notifications...");
+
+        // Importa Firebase dinámicamente solo si hay conexión
+        const { initializeApp } = await import("https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js");
+        const { getMessaging, onMessage } = await import("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js");
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyCPtGrKaRYMUxDx6SXIQvlewLNxyapnxcM",
+            authDomain: "dosxdos-app.firebaseapp.com",
+            projectId: "dosxdos-app",
+            storageBucket: "dosxdos.app.firebasestorage.app",
+            messagingSenderId: "52849872855",
+            appId: "1:52849872855:web:3d68fa7ed8dd785fe14592",
+            measurementId: "G-ZG5EKJDHL5"
+        };
+
+        const app = initializeApp(firebaseConfig);
+        const messaging = getMessaging(app);
+
+        // Manejar mensajes en primer plano
+        onMessage(messaging, (payload) => {
+            console.log('[Firebase web push notifications] Mensaje en primer plano:', payload);
+
+            // Extraer datos con valores por defecto si vienen nulos o indefinidos
+            const title = payload.data.title || "Nueva Notificación";
+            const body = payload.data.body || "Tienes una nueva notificación, por favor revísala en cuanto puedas.";
+            const icon = payload.data.icon || "https://dosxdos.app.iidos.com/img/dosxdoslogoNuevoRojo.png";
+            const click_action = payload.data.click_action || "https://dosxdos.app.iidos.com/notificaciones.html";
+
+            // Crear un string bien formado
+            const mensaje = "Tienes una nueva notificación: " + title + ": " + body;
+
+            scrollToTop();
+
+            // Mostrar notificación en la UI si el usuario tiene permisos activados
+            if (Notification.permission === "default") {
+                Notification.requestPermission().then((permission) => {
+                    if (permission === "granted") {
+                        const notificacionSistemaOperativo = new Notification(title, { body, icon });
+                        notificacionSistemaOperativo.onclick = () => {
+                            window.location.href = click_action;
+                        };
+                    }
+                });
+            } else if (Notification.permission === "granted") {
+                const notificacionSistemaOperativo = new Notification(title, { body, icon });
+                notificacionSistemaOperativo.onclick = () => {
+                    window.location.href = click_action;
+                };
+            }
+
+            localStorage.setItem('mensaje', 'Tienes una nueva notificación que debes revisar: ' + title + ': ' + body);
+            location.reload();
+
+        });
+
+        console.warn('Firebase Web push notifications cargado exitosamente')
+    } catch (error) {
+        console.error('No es posible cargar Firebase web push notifications en este dispositivo')
+        console.error(error);
+    }
+
+}
+
+// Cargar Firebase al inicio si hay conexión
+if (navigator.onLine) loadFirebase();
+
